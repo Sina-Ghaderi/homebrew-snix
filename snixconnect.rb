a7052eed57c30cb5965ca707c658cfb3f261f59c
Sina Ghaderi <32870524+Sina-Ghaderi@users.noreply.github.com>
2025-10-29 22:24:20 +0330

    Update snixconnect.rb

--- snixconnect.rb
+++ snixconnect.rb
@@ -2,7 +2,7 @@ class Mytool < Formula
   desc "SnixConnect VPN client for MacOS"
   homepage "https://github.com/sina-ghaderi/homebrew-snix"
   url "https://github.com/sina-ghaderi/homebrew-snix/releases/download/v0.11.4/snixconnect-0.11.4-darwin-amd64.tar.gz"
-  sha256 "39028f174c7552ffbaffcbc1ef03e50f7997fff32ab931ef1963b17db975cb9b"
+  sha256 "95c82472443a88cfaa3e4d170d5987abfd268f84e56b3919c8e78afcd0f76a23"
   license "MIT"
 
   def install
